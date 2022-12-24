e1e02ae90b64ffb76e34c09e81c86c04224a3fd6
Athanasios Grivas <nasos.grivas@gmail.com>
2022-12-24 20:48:05 +0200

    Make the calculator work

--- js/calculator.js
+++ js/calculator.js
@@ -1,33 +1,39 @@
-const digitButtons = document.querySelectorAll(".digits button");
-const display = document.querySelector(".display");
-let displayValue = 0;
+const displayField = document.querySelector(".display");
+const digitBtns = document.querySelectorAll(".digits button");
+const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector("#equals");
+const clearBtn = document.querySelector("#clear");
+let previousDisplayValue = 0;
 let currentOperator;
+let newInput = true;
 
-function displayDigit(digit) {
-  display.innerText = digit;
-  displayValue = digit;
+function display(digit) {
+    displayField.innerText = digit;
 }
 
-for (let digitButton of digitButtons) {
-  let digit = digitButton.innerText;
-  digitButton.addEventListener("click", () => displayDigit(digit));
+function displayNumber(digit) {
+  if (newInput) {
+    display(digit);
+    newInput = false;
+  } else {
+    display(displayField.innerText + digit);
+  }
 }
 
 function add(a, b) {
-  return a + b;
+  return +a + +b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return +a - +b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return +a * +b;
 }
 
 function divide(a, b) {
-  console.log((a / b).toFixed(2));
-  return (a / b).toFixed(2);
+  return Math.trunc((+a / +b) * 100) / 100;
 }
 
 function operate(operator, a, b) {
@@ -40,7 +46,36 @@ function operate(operator, a, b) {
       return multiply(a, b);
     case "/":
       return divide(a, b);
+    case "=":
+      return b;
     default:
-      return "unknown operator";
+      console.log("unknown operator");
   }
 }
+
+function cache(operator) {
+  currentOperator = operator;
+  previousDisplayValue = displayField.innerText;
+  newInput = true;
+}
+
+function displayResult(operator, a, b) {
+  let result = operate(operator, a, b);
+  display(result);
+  currentOperator = "=";
+  newInput = true;
+}
+
+for (let digitBtn of digitBtns) {
+  let digit = digitBtn.innerText;
+  digitBtn.addEventListener("click", () => displayNumber(digit));
+}
+
+for (let operatorBtn of operatorBtns) {
+  let operator = operatorBtn.innerText;
+  operatorBtn.addEventListener("click", () => cache(operator));
+}
+
+equalsBtn.addEventListener("click", () =>
+  displayResult(currentOperator, previousDisplayValue, displayField.innerText)
+);
