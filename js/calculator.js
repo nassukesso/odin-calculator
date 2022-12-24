796a181437de5d15118140851fd8182ef828807f
Athanasios Grivas <nasos.grivas@gmail.com>
2022-12-24 17:20:24 +0200

    Create functions for the basic math operators

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+const add = function (a, b) {
+  return a + b;
+}
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+}
+
+const divide = function (a, b) {
+  console.log((a / b).toFixed(2));
+  return (a / b).toFixed(2);
+}
