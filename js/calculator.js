9223aff2760597c9f356157896fae73277ea2ada
Athanasios Grivas <nasos.grivas@gmail.com>
2022-12-24 17:24:21 +0200

    Create operate function

--- js/calculator.js
+++ js/calculator.js
@@ -14,3 +14,18 @@ const divide = function (a, b) {
   console.log((a / b).toFixed(2));
   return (a / b).toFixed(2);
 }
+
+const operate = function (operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return "unknown operator"
+  }
+}
